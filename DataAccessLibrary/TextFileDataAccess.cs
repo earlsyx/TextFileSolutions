59e8f25b1caafe9b3869334e2d97ec805acd09dc
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-08-29 08:48:57 +0800

    - contact model - textfiledataaccess   - read lines,  - split string  - validation  - add output  - WriteAllRecords - csv
    Program.cs
     - configuration,
     - initialize to read appsettings.json
     - build

---
+++ DataAccessLibrary/TextFileDataAccess.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO
+namespace DataAccessLibrary
+{
+    public class TextFileDataAccess
+    {
+        public List<ContactModel> ReadAllRecords(string textFile)
+        {
+            var lines = File.ReadAllLines(textFile);
+            List<ContactModel> output = new List<ContactModel>();
+
+            foreach (var line in lines)
+            {
+                ContactModel c = new ContactModel();
+                var vals = line.Split(',');
+
+                if (vals.Length < 4)
+                {
+                    throw new Exception($"Invalid Row of data: {line}");
+                }
+
+                c.FirstName = vals[0];
+
+                output.Add(c);
+            }
+        }
+
+        public void WriteAllRecords(List<ContactModel> contacts, string textFile)
+        {
+            //CSV, Tim,Corey,555-1212 - downside cant have comma in value. Pile Separation ||
+            // Working with strings can be very difficult
+            // Everything time you add string together 
+            // - creates new memory space for that, very quickly become very expensive
+            // string builder allows you to do this more efficiently, negative effects, memor store front?
+            List<string> lines = new List<string>();
+            // efficently handling adding string.
+            foreach (var c in contacts)
+            {
+                lines.Add($"{c.FirstName}, {c.LastName},{String.Join(';', c.EmailAddresses)}, {String.Join(';', c.PhoneNumbers)}");
+            }
+
+            File.WriteAllLines(textFile, lines);
+
+        }
+
+    }
+}
--- TextFileUI/Program.cs
+++ TextFileUI/Program.cs
@@ -1,12 +1,34 @@
-﻿namespace TextFileUI
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace TextFileUI
 {
     internal class Program
     {
+        private static IConfiguration _config;
+        private static string textFile;
         static void Main(string[] args)
         {
+            InitilizeConfiguration();
+            textFile = _config.GetValue<string>("TextFile");
+
+
+
+
             Console.ReadLine();
         }
 
+        private static void  InitilizeConfiguration()
+        {
+            //(string endpointUrl, string primaryKey, string databaseName, string containerName) output;
+
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
+        }
          
     }
 }
